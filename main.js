9d6a4086c76258943044a7b28a08b08ae70b30f4
JorgeSL <salj8009@gmail.com>
2022-06-06 18:27:59 -0500

    Se dio estilos css

--- main.js
+++ main.js
@@ -9,12 +9,20 @@ function calcularPrecioCondescuento(precio , descuento) {
     return precioConDescuento;
 }
 
+const cupones = ["nuevo" , "lealtad" , "sorpresa"];
+
+
 const inputprecio = document.getElementById('inputprecio');
 const inputdescuento = document.getElementById('inputdescuento');
 
 const btnCalcular = document.getElementById('btnCalcular');
 
 const preciorResultado = document.getElementById('precio-resultado');
+const divcupones = document.getElementById('div-cupones');
+const fragment = document.createDocumentFragment();
+
+const imagenpreciofinal = document.getElementById('imagen-precio-final');
+
 
 btnCalcular.addEventListener('click' , () =>{
     const precioValue = inputprecio.value;
@@ -22,7 +30,17 @@ btnCalcular.addEventListener('click' , () =>{
 
     const precioConDescuento = calcularPrecioCondescuento(precioValue, descuentoValue);
 
-    preciorResultado.innerText = `el precio con descuento es $: ${precioConDescuento}`;
+    preciorResultado.innerText = `El precio con descuento es de $: ${precioConDescuento}`;
+    const parrafo = document.createElement('P');
+    const btnCupones = document.createElement('button');
+    parrafo.textContent = `Tienes cupones ?`;
+    btnCupones.textContent = `Aplicalos Aqui.`;
+    btnCupones.id = `MIBTN`;
+    fragment.appendChild(parrafo);
+    fragment.appendChild(btnCupones);
+
+    divcupones.appendChild(fragment);
+   
 });
 
 /*console.log({
